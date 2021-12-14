32d3689c5ae76ba3c7b9b6bb895179cc117dcbaf
Hilful <hilful25@gmail.com>
2021-12-15 01:50:36 +0600

    setting in pages is not complete yet ,so currently working on it

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/home/single/Single";
 import Write from "./pages/home/Write/Write";
+import Settings from "./pages/Settings/Settings";
 
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Write/>
+      <Settings/>
       
       
     </>
---
+++ src/pages/Settings/Settings.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sidebar from '../../components/sidebar/Sidebar';
+import "./Settings.css";
+const Settings = () => {
+    return (
+        <div className="settings">
+            <div className="settingsWrapper">
+                
+            </div>
+            <Sidebar/>
+        </div>
+    );
+};
+
+export default Settings;
