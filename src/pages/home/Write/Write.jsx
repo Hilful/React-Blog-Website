47df5cb6433bf104dd30838c462730bac435274a
Hilful <hilful25@gmail.com>
2021-12-15 01:33:13 +0600

    new write page added to blog site for writing post as medium

---
+++ src/pages/home/Write/Write.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./Write.css";
+
+const Write = () => {
+    return (
+        <div className = "write">
+                <img
+                 className = "writeImage"
+                 src=""
+                  alt="" />
+            <form className="writeForm">
+                <div className="writeFormGroup">
+                    <label htmlFor="fileInput">
+                        <i className="writeIcon  fas fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style= {{display: "none"}} />
+                    <input type="text" placeholder="Title" className="writeInput" autoFocus = {true} />
+                </div>
+                <div className="writeFormGroup">
+                    <textarea placeholder="Tell Your Story...." type="text" className="writeInput writeText"></textarea>
+                </div>
+                <button className="writeSubmit">Publish</button>
+            </form>
+
+        </div>
+    );
+};
+
+export default Write;
